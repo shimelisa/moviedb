e9a2e8959a98a6773b7c7cd1629d1dafce7598b0
shimelisa <shiaduara@gmail.com>
2026-05-19 21:42:43 -0400

    updated MovieInstance & requestUrls with ACCESS_TOKEN instead of API_KEY

--- src/Utility/MovieInstance.js
+++ src/Utility/MovieInstance.js
@@ -1,9 +1,18 @@
-import axios from 'axios'
+import axios from "axios";
+
+
+// const movieInstance = axios.create({
+//   baseURL: "https://api.themoviedb.org/3",
+// });
 
 const movieInstance = axios.create({
-      baseURL: "https://api.themoviedb.org/3"
+  baseURL: "https://api.themoviedb.org/3",
+  headers: {
+    Accept: "application/json",
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_ACCESS_TOKEN}`,
+  },
 });
 
-export {movieInstance}
+export { movieInstance };
 // https://api.themoviedb.org/3/account/{account_id}/rated/tv
 // https://api.themoviedb.org/3/discover/movie?api_key=865b113325881d72a32c436870a5d6b2&with_genres=99
--- src/Utility/requestUrls.js
+++ src/Utility/requestUrls.js
@@ -1,13 +1,25 @@
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
+// const requests = {
+//   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+//   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+//   fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+//   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+//   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+//   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+//   fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+//   fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+// };
+
 const requests = {
-  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-  fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fetchTrending: "/trending/all/week?language=en-US",
+  fetchNetflixOriginals: "/discover/tv?with_networks=213",
+  fetchTopRatedMovies: "/movie/top_rated?language=en-US",
+  fetchActionMovies: "/discover/movie?with_genres=28",
+  fetchComedyMovies: "/discover/movie?with_genres=35",
+  fetchHorrorMovies: "/discover/movie?with_genres=27",
+  fetchRomanceMovies: "/discover/movie?with_genres=10749",
+  fetchDocumentaries: "/discover/movie?with_genres=99",
 };
+
 export default requests;
